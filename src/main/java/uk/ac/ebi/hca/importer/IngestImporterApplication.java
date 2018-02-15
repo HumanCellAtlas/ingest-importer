235d12ea3a6cbca7f7e7339243f5f9c6a9f467f8
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-02-15 10:37:15 +0000

    Created Ingest Importer project.

---
+++ src/main/java/uk/ac/ebi/hca/importer/IngestImporterApplication.java
@@ -0,0 +1,12 @@
+package uk.ac.ebi.hca.importer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngestImporterApplication.class, args);
+	}
+}
