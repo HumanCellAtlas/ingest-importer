9df4a036568ce0c3e36aa5b80d026b856c19496f
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-02-27 10:56:14 +0000

    Added code to compile all results from all worksheet importers.

--- src/main/java/uk/ac/ebi/hca/importer/excel/WorkbookImporter.java
+++ src/main/java/uk/ac/ebi/hca/importer/excel/WorkbookImporter.java
@@ -2,18 +2,36 @@ package uk.ac.ebi.hca.importer.excel;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.poi.ss.usermodel.Workbook;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class WorkbookImporter {
 
     private final ObjectMapper objectMapper;
+
+    private final Map<String, WorksheetImporter> registry = new HashMap<>();
 
     public WorkbookImporter(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
     }
 
     public JsonNode importFrom(Workbook workbook) {
-        return objectMapper.createObjectNode();
+        ObjectNode json = objectMapper.createObjectNode();
+        workbook.iterator().forEachRemaining(worksheet -> {
+            JsonNode worksheetJson = registry.get(worksheet.getSheetName()).importFrom(worksheet);
+            worksheetJson.fieldNames().forEachRemaining(fieldName -> {
+                json.set(fieldName, worksheetJson.get(fieldName));
+            });
+        });
+        return json;
+    }
+
+    public WorkbookImporter register(String sheetName, WorksheetImporter worksheetImporter) {
+        registry.put(sheetName, worksheetImporter);
+        return this;
     }
 
 }
--- src/test/java/uk/ac/ebi/hca/importer/excel/WorkbookImporterTest.java
+++ src/test/java/uk/ac/ebi/hca/importer/excel/WorkbookImporterTest.java
@@ -1,13 +1,21 @@
 package uk.ac.ebi.hca.importer.excel;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.jayway.jsonassert.JsonAssert;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.junit.Test;
 
+import java.util.List;
+
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
@@ -16,7 +24,7 @@ public class WorkbookImporterTest {
     private ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
-    public void testImportFrom() {
+    public void testImportFrom() throws Exception {
         //given:
         Sheet productSheet = mock(Sheet.class);
         doReturn("product").when(productSheet).getSheetName();
@@ -28,13 +36,67 @@ public class WorkbookImporterTest {
         doReturn(asList(productSheet, orderSheet).iterator()).when(workbook).iterator();
 
         //and:
+        ObjectNode productJson = objectMapper.createObjectNode();
+        ArrayNode productArray = productJson.putArray("products");
+        List<Product> products = asList(new Product("P001", "Milk", 1.09),
+                new Product("P002", "Eggs", 1.75), new Product("P003", "Cereal", 2.00));
+        products.forEach(productArray::addPOJO);
+        WorksheetImporter productImporter = mock(WorksheetImporter.class);
+        doReturn(productJson).when(productImporter).importFrom(any(Sheet.class));
+
+        //and:
+        ObjectNode orderJson = objectMapper.createObjectNode();
+        ArrayNode orderArray = orderJson.putArray("orders");
+        List<Order> orders = asList(new Order("O001", "P001", 2), new Order("O002", "P001", 1),
+                new Order("O003", "P003", 3));
+        orders.forEach(orderArray::addPOJO);
+        WorksheetImporter orderImporter = mock(WorksheetImporter.class);
+        doReturn(orderJson).when(orderImporter).importFrom(any(Sheet.class));
+
+        //and:
         WorkbookImporter workbookImporter = new WorkbookImporter(objectMapper);
+        workbookImporter.register(productSheet.getSheetName(), productImporter)
+                .register(orderSheet.getSheetName(), orderImporter);
 
         //when:
         JsonNode workbookJson = workbookImporter.importFrom(workbook);
 
         //then:
         assertThat(workbookJson).isNotNull();
+        JsonAssert.with(objectMapper.writeValueAsString(workbookJson))
+                .assertThat("$.products", hasSize(products.size()))
+                .assertThat("$.orders", hasSize(orders.size()));
+    }
+
+    private static class Product {
+
+        @JsonProperty("product_id")
+        String productId;
+        String name;
+        Double price; //ideally BigDecimal, but simplified for testing
+
+        Product(String productId, String name, Double price) {
+            this.productId = productId;
+            this.name = name;
+            this.price = price;
+        }
+
+    }
+
+    private static class Order {
+
+        @JsonProperty("order_number")
+        String orderNumber;
+        @JsonProperty("product_id")
+        String productId;
+        Integer quantity;
+
+        public Order(String orderNumber, String productId, Integer quantity) {
+            this.orderNumber = orderNumber;
+            this.productId = productId;
+            this.quantity = quantity;
+        }
+
     }
 
 }
