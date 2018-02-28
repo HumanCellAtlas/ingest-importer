7a70b87cac36755e49bd4bf1d9a35131435dadc1
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-02-28 13:43:22 +0000

    Added predefined values for project_core module.

--- src/main/java/uk/ac/ebi/hca/importer/SpreadsheetImporterConfiguration.java
+++ src/main/java/uk/ac/ebi/hca/importer/SpreadsheetImporterConfiguration.java
@@ -33,17 +33,23 @@ public class SpreadsheetImporterConfiguration {
                 .put("describedBy", "https://schema.humancellatlas.org/type/project/5.0.0/project")
                 .put("schema_version", "5.0.0")
                 .put("schema_type", "project");
-        return new WorksheetImporter(objectMapper, "projects",
-                new WorksheetMapping()
-                        .map("Project shortname", "project_core.project_shortname", STRING)
-                        .map("Project title", "project_core.project_title", STRING)
-                        .map("Project description", "project_core.project_description", STRING)
-                        .map("Supplementary files", "supplementary_files", STRING_ARRAY)
-                        .map("INSDC project accession", "insdc_project", STRING)
-                        .map("GEO series accession", "geo_series", STRING)
-                        .map("ArrayExpress accession", "array_express_investigation", STRING)
-                        .map("INSDC study accession", "insdc_study", STRING),
-                predefinedValues);
+        WorksheetImporter importer = new WorksheetImporter(objectMapper, "projects", new
+                WorksheetMapping()
+                .map("Project shortname", "project_core.project_shortname", STRING)
+                .map("Project title", "project_core.project_title", STRING)
+                .map("Project description", "project_core.project_description", STRING)
+                .map("Supplementary files", "supplementary_files", STRING_ARRAY)
+                .map("INSDC project accession", "insdc_project", STRING)
+                .map("GEO series accession", "geo_series", STRING)
+                .map("ArrayExpress accession", "array_express_investigation", STRING)
+                .map("INSDC study accession", "insdc_study", STRING), predefinedValues);
+        ObjectNode coreModuleValues = objectMapper
+                .createObjectNode()
+                .put("describedBy",
+                        "https://schema.humancellatlas.org/core/project/5.0.0/project_core")
+                .put("schema_version", "5.0.0");
+        importer.defineValuesFor("project_core", coreModuleValues);
+        return importer;
     }
 
     @Bean
