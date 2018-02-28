f7f1154a53e99d9041bf738db4a0f8a5e49586e8
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-02-28 13:40:00 +0000

    Updated code for registering worksheet importers to workbook importer.

--- src/main/java/uk/ac/ebi/hca/importer/SpreadsheetImporterConfiguration.java
+++ src/main/java/uk/ac/ebi/hca/importer/SpreadsheetImporterConfiguration.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.ContextStartedEvent;
 import org.springframework.context.event.EventListener;
 import uk.ac.ebi.hca.importer.excel.WorkbookImporter;
 import uk.ac.ebi.hca.importer.excel.WorksheetImporter;
 import uk.ac.ebi.hca.importer.excel.WorksheetMapping;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static uk.ac.ebi.hca.importer.excel.CellDataType.STRING;
@@ -50,14 +52,17 @@ public class SpreadsheetImporterConfiguration {
     }
 
     @EventListener
-    public void importerRegistrar(ContextStartedEvent event) {
+    public void importerRegistrar(ContextRefreshedEvent event) {
         ApplicationContext applicationContext = event.getApplicationContext();
         WorkbookImporter workbookImporter = applicationContext.getBean(WorkbookImporter.class);
         Pattern namePattern = Pattern.compile("importer\\.([\\p{Alpha}_]+)");
         applicationContext.getBeansOfType(WorksheetImporter.class)
                 .forEach((name, importer) -> {
-                    String sheetName = namePattern.matcher(name).group(1);
-                    workbookImporter.register(sheetName, importer);
+                    Matcher matcher = namePattern.matcher(name);
+                    if (matcher.matches()) {
+                        String sheetName = matcher.group(1);
+                        workbookImporter.register(sheetName, importer);
+                    }
                 });
     }
 
