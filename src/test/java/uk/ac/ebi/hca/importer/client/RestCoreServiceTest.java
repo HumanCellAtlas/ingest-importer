bdd6ba86cf64e9832fcefe4d96cc839d24a89696
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-03-06 16:24:19 +0000

    Initially set up code for RestCoreService.

--- src/test/java/uk/ac/ebi/hca/importer/client/RestCoreServiceTest.java
+++ src/test/java/uk/ac/ebi/hca/importer/client/RestCoreServiceTest.java
@@ -1,27 +1,57 @@
 package uk.ac.ebi.hca.importer.client;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
+import org.springframework.test.web.client.response.MockRestResponseCreators;
+import org.springframework.web.client.RestTemplate;
+import uk.ac.ebi.hca.core.CoreService;
+import uk.ac.ebi.hca.core.RestCoreService;
 
 import java.io.IOException;
 
-import static junit.framework.TestCase.assertNotNull;
 import static junit.framework.TestCase.assertTrue;
-import static org.junit.Assert.assertFalse;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 import static uk.ac.ebi.hca.importer.client.IngestApiClient.EntityType.PROJECT;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class IngestApiClientTest {
+public class RestCoreServiceTest {
+
+    @Configuration
+    static class TestConfiguration {
+
+        @Bean
+        RestTemplate restTemplate() {
+            return new RestTemplate();
+        }
+
+        @Bean
+        CoreService coreService() {
+            return new RestCoreService();
+        }
+
+    }
 
     private final String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik1qTkZRa1U0UWtGRlFqUXdRVVEwUlVZNFJqWkZOa1kxUmtVMk9EWXdORE5EUWprd1FrRTFPQSJ9.eyJpc3MiOiJodHRwczovL2RhbmllbHZhdWdoYW4uZXUuYXV0aDAuY29tLyIsInN1YiI6Ilpkc29nNG5EQW5oUTk5eWlLd01RV0FQYzJxVURsUjk5QGNsaWVudHMiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjgwODAiLCJpYXQiOjE1MTk5ODgyNTMsImV4cCI6MTUyMDA3NDY1MywiYXpwIjoiWmRzb2c0bkRBbmhROTl5aUt3TVFXQVBjMnFVRGxSOTkiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.FJINm7PDG8s3qlzFHmk3Os9vdS1mDdXTsXkDKNZ4z9rRLfd0IICu7b2g81d7YFv6dIrtoCsN3GpE3A4CSysCo3bdtQEZmvyRE1ZQbcqhEGh2sIDqIVd9Q_T_e3eaGBJbs2x0GHnKk-fXtWItjzo5LNddRK1QEGXmggb5eDL60Ms4HCR82YxB84IeshbX5grZvzbLgYbQMjaoPA5aBngcEMrlyLFLccKAGMvu8Gc2GGP9Bpsx5hEZbDGnT4IPag0Sk7Ik8W_jQEcz0AtMfKlFekyYWKwM_22DDsgYm9RQSNVhFY8RKEVFLP-h4wSlncIZ7OWmbkMzSThW9dn172h4Gg";
 
     private final IngestApiClient ingestApiClient = new IngestApiClient();
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private CoreService coreService;
 
     @Ignore
     //TODO define this test
@@ -40,6 +70,23 @@ public class IngestApiClientTest {
     @Test
     public void testNothing() {
         //TODO remove this
+    }
+
+    @Test
+    public void testPrepareSubmission() {
+        //given:
+        MockRestServiceServer server = MockRestServiceServer.bindTo(restTemplate).build();
+
+        //and:
+        server.expect(requestTo("http://foo.bar/submissions"))
+                .andExpect(method(HttpMethod.POST))
+                .andRespond(withSuccess());
+
+        //when:
+        coreService.prepareSubmission();
+
+        //then:
+        server.verify();
     }
 
 }
