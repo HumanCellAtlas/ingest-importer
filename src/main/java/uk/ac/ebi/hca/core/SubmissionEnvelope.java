67efae4b48c659a078da22150c01a58f9df9205e
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-03-07 15:52:31 +0000

    Defined put method for Dataset.

--- src/main/java/uk/ac/ebi/hca/core/SubmissionEnvelope.java
+++ src/main/java/uk/ac/ebi/hca/core/SubmissionEnvelope.java
@@ -2,10 +2,16 @@ package uk.ac.ebi.hca.core;
 
 public class SubmissionEnvelope {
 
+    private Dataset dataset;
+
     private String submissionUrl;
 
     public SubmissionEnvelope(String submissionUrl) {
         this.submissionUrl = submissionUrl;
+    }
+
+    public void put(Dataset dataset) {
+        this.dataset = dataset;
     }
 
 }
