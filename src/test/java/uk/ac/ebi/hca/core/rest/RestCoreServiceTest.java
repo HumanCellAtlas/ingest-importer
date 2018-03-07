acbafcfd0081922da32a2560a9f8410758b0a5bf
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-03-07 14:20:18 +0000

    Removed unused tests.

--- src/test/java/uk/ac/ebi/hca/core/rest/RestCoreServiceTest.java
+++ src/test/java/uk/ac/ebi/hca/core/rest/RestCoreServiceTest.java
@@ -2,12 +2,10 @@ package uk.ac.ebi.hca.core.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -16,16 +14,11 @@ import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
 import uk.ac.ebi.hca.core.CoreService;
 import uk.ac.ebi.hca.core.SubmissionEnvelope;
-import uk.ac.ebi.hca.importer.client.IngestApiClient;
 
-import java.io.IOException;
-
-import static junit.framework.TestCase.assertTrue;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
-import static uk.ac.ebi.hca.importer.client.IngestApiClient.EntityType.PROJECT;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(properties={
@@ -35,10 +28,6 @@ public class RestCoreServiceTest {
 
     private static final String URL_SUBMISSIONS = "http://core.sample.com/submissions";
 
-    private final String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik1qTkZRa1U0UWtGRlFqUXdRVVEwUlVZNFJqWkZOa1kxUmtVMk9EWXdORE5EUWprd1FrRTFPQSJ9.eyJpc3MiOiJodHRwczovL2RhbmllbHZhdWdoYW4uZXUuYXV0aDAuY29tLyIsInN1YiI6Ilpkc29nNG5EQW5oUTk5eWlLd01RV0FQYzJxVURsUjk5QGNsaWVudHMiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjgwODAiLCJpYXQiOjE1MTk5ODgyNTMsImV4cCI6MTUyMDA3NDY1MywiYXpwIjoiWmRzb2c0bkRBbmhROTl5aUt3TVFXQVBjMnFVRGxSOTkiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.FJINm7PDG8s3qlzFHmk3Os9vdS1mDdXTsXkDKNZ4z9rRLfd0IICu7b2g81d7YFv6dIrtoCsN3GpE3A4CSysCo3bdtQEZmvyRE1ZQbcqhEGh2sIDqIVd9Q_T_e3eaGBJbs2x0GHnKk-fXtWItjzo5LNddRK1QEGXmggb5eDL60Ms4HCR82YxB84IeshbX5grZvzbLgYbQMjaoPA5aBngcEMrlyLFLccKAGMvu8Gc2GGP9Bpsx5hEZbDGnT4IPag0Sk7Ik8W_jQEcz0AtMfKlFekyYWKwM_22DDsgYm9RQSNVhFY8RKEVFLP-h4wSlncIZ7OWmbkMzSThW9dn172h4Gg";
-
-    private final IngestApiClient ingestApiClient = new IngestApiClient();
-
     @Autowired
     private RestTemplate restTemplate;
 
@@ -46,25 +35,6 @@ public class RestCoreServiceTest {
     private CoreService coreService;
 
     private ObjectMapper objectMapper = new ObjectMapper();
-
-    @Ignore
-    //TODO define this test
-    public void test_create_submission() throws IOException {
-        String result = ingestApiClient.createSubmission(token);
-        assertTrue(result.startsWith("http://localhost:8080/submissionEnvelopes/"));
-    }
-
-    @Ignore
-    //TODO define this test
-    public void test_create_submission_then_project() {
-        String submissionUrl = ingestApiClient.createSubmission(token);
-        String response = ingestApiClient.createEntity(token, submissionUrl, PROJECT, "{}");
-    }
-
-    @Test
-    public void testNothing() {
-        //TODO remove this
-    }
 
     @Test
     public void testPrepareSubmission() throws Exception {
