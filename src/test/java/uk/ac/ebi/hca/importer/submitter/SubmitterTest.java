1a2f5233c77dc81057d7556b45a30adffbe646c9
Daniel Vaughan <dan@danielvaughan.com>
2018-03-11 20:26:13 +0000

    Disabled test for large spreadsheet

--- src/test/java/uk/ac/ebi/hca/importer/submitter/SubmitterTest.java
+++ src/test/java/uk/ac/ebi/hca/importer/submitter/SubmitterTest.java
@@ -1,6 +1,7 @@
 package uk.ac.ebi.hca.importer.submitter;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +20,7 @@ public class SubmitterTest {
     private Submitter submitter = new Submitter(ingestApiClient);
 
     @Test
+    @Ignore
     public void given_Q4DemoSS2Metadata_v5_json_ingest_successfully() {
         JsonNode jsonNode = FileUtil.buildJsonNodeFromUrl(Q4DemoSS2Metadata_v5_EXPECTED_JSON_URL);
         submitter.submit(ingestApiClient.requestAuthenticationToken(), jsonNode);
